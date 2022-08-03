ce8eed11e2a93fb7df3450f2a688f1ad7cae7281
Siarhei Stsiapanau <siarhei.stsiapanau@sap.com>
2022-08-03 21:27:22 +0400

    change API path to import the file

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://83hdspqk71.execute-api.eu-west-1.amazonaws.com',
 	bff: 'https://9kpocb3x23.execute-api.eu-west-1.amazonaws.com',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
