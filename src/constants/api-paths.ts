c4d7f0a0343dc2560f23aaadc6156285a02ec394
Sergey Stepanov <siargei.stepanov@gmail.com>
2022-08-24 10:24:03 +0400

    Added auth headers for import API

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://83hdspqk71.execute-api.eu-west-1.amazonaws.com',
+	import: 'https://l24x60bwi5.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://9kpocb3x23.execute-api.eu-west-1.amazonaws.com',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
